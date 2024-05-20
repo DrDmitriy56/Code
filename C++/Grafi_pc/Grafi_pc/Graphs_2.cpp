e3ea7d7d8814876ad1fdc3b8f2c3579842c924fe
Dmitriy Da <dimadav56@gmail.com>
2024-05-20 20:58:50 +0300

    Grafi
    graphy

---
+++ C++/Grafi_pc/Grafi_pc/Graphs_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int n, num, counter = 0;
+	cin >> n;
+
+	//vector <vector<int>> graph(n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> num;
+			if (num == 1)
+				counter += 1;
+		}
+	}
+	cout << counter;
+}
