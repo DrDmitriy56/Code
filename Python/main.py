01c13fe190f1bc6c717f43592dc0929020cb6b7f
Dmitriy Da <dimadav56@gmail.com>
2024-06-08 13:47:20 +0300

    correct

--- Python/main.py
+++ Python/main.py
@@ -1,22 +1,21 @@
 import matplotlib.pyplot as plt
 import math
 
-def Area(y, Step, Amount):
+def Area(y, Step, length):
     area = 0
-    for i in range(Amount):
+    for i in range(length):
         area += Step * y[i]
     return area
 
-def fill_sin(Amount, Step, Path, x, y):
-    while Amount > Path:
+def fill_sin(length, Step, Path, x, y):
+    while length > Path:
         x.append(Path)
-        y.append(math.sin(Path))
+        y.append(1)
         Path += Step
     plt.plot(x, y)
     Path = 0
-
-def fill_cos(Amount, Step, Path, x, y):
-    while Amount > Path:
+def fill_cos(length, Step, Path, x, y):
+    while length > Path:
         x.append(Path)
         y.append(math.cos(Path))
         Path += Step
@@ -28,14 +27,14 @@ y = []
 x1 =[]
 y1 =[]
 
-Amount = int(input('X length - '))
-Step = float(input('Step - '))
+length = int(input('X length - '))
+Step = float(input('Step - ')) #Цена деления
 Path = 0
 
-fill_sin(Amount, Step, Path, x, y)
-fill_cos(Amount, Step, Path, x1, y1)
+fill_sin(length, Step, Path, x, y)
+fill_cos(length, Step, Path, x1, y1)
 
-print('Area 1gr -', Area(y, Step, Amount))
-print('Area 2gr -', Area(y1, Step, Amount))
+print('Area 1gr -', Area(y, Step, length))
+print('Area 2gr -', Area(y1, Step, length))
 
 plt.show()
