2c1b02240defb0264bfb0c97ad5be5c26e6ef7e6
Dmitriy Da <dimadav56@gmail.com>
2024-06-08 15:28:05 +0300

    beauty

--- Python/Integrirovanie.py
+++ Python/Integrirovanie.py
@@ -1,17 +1,23 @@
 import math
 
-area = 0
 skipFst = False
+
+area = 0
 xLength = 10
+step = 0.1
+
 def f(x):
     return math.sin(x)
-def integrationTrapezoid(fstValue, sndValue, delay):
-    global area
-    area += ((fstValue + sndValue) / 2) * delay
 
-for x in range(xLength + 1):
+def integrationTrapezoid(fstValue, sndValue):
+    global area
+    global step
+    area += ((fstValue + sndValue) / 2) * step
+
+
+for x in range(int(xLength/step) + 1):
     if skipFst:
-        integrationTrapezoid(f(x-1), f(x), 1)
+        integrationTrapezoid(f((x - 1) * step), f(x * step))
     else:
         skipFst = True
 
